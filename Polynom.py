4c5f3adfd300bbf184427027f6c7ca0456105f51
whosFritz <97552289+whosFritz@users.noreply.github.com>
2023-01-09 10:53:36 +0100

    erg zu ergebnis geändert

--- Polynom.py
+++ Polynom.py
@@ -12,38 +12,38 @@ class Polynom:
         max_grad = max(self.grad(), other.grad())
         self_koeffizienten = self.koeffizienten + [0] * (max_grad - self.grad())
         other_koeffizienten = other.koeffizienten + [0] * (max_grad - other.grad())
-        erg_koeffizienten = [0] * (max_grad + 1)
+        ergebnis_koeffizienten = [0] * (max_grad + 1)
         for i in range(max_grad + 1):
-            erg_koeffizienten[i] = self_koeffizienten[i] + other_koeffizienten[i]
-        return Polynom(erg_koeffizienten)
+            ergebnis_koeffizienten[i] = self_koeffizienten[i] + other_koeffizienten[i]
+        return Polynom(ergebnis_koeffizienten)
 
     def __mul__(self, other):
-        erg_koeffizienten = [0] * (self.grad() + other.grad() + 1)
+        ergebnis_koeffizienten = [0] * (self.grad() + other.grad() + 1)
         for i in range(self.grad() + 1):
             for j in range(other.grad() + 1):
-                erg_koeffizienten[i + j] += self.koeffizienten[i] * other.koeffizienten[j]
-        return Polynom(erg_koeffizienten)
+                ergebnis_koeffizienten[i + j] += self.koeffizienten[i] * other.koeffizienten[j]
+        return Polynom(ergebnis_koeffizienten)
 
     def auswerten(self, x):
-        erg = 0
+        ergebnis = 0
         for i in range(self.grad() + 1):
-            erg += self.koeffizienten[i] * x ** i
-        return erg
+            ergebnis += self.koeffizienten[i] * x ** i
+        return ergebnis
     
     def __str__(self):
-        erg = ""
+        ergebnis = ""
         for i, koeffizient in enumerate(self.koeffizienten):
             if koeffizient == 0:
                 continue
             if koeffizient < 0:
-                erg += " - "
+                ergebnis += " - "
             else:
                 if i > 0 or (i == 0 and koeffizient != 1):
-                    erg += " + "
+                    ergebnis += " + "
             if abs(koeffizient) != 1 or i == 0:
-                erg += str(abs(koeffizient))
+                ergebnis += str(abs(koeffizient))
             if i > 0:
-                erg += "x"
+                ergebnis += "x"
                 if i > 1:
-                    erg += "^" + str(i)
-        return erg
+                    ergebnis += "^" + str(i)
+        return ergebnis
